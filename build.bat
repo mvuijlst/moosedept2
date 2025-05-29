02c6e84ae46dc918c2cf96b1aacc390fd313eed4
Michel Vuijlsteke <michel@zog.org>
2025-05-29 17:12:49 +0200

    build

--- build.bat
+++ build.bat
@@ -3,8 +3,7 @@ cd public
 call firebase deploy
 cd ..
 ..\hugo.exe --baseURL "http://users.ugent.be/~mvuijlst/" 
+powershell -ExecutionPolicy Bypass -File smart-copy.ps1
 git add *
 git commit -m "build"
 git push
-REM xcopy public \\files\mvuijlst\www\users /s /y
-powershell -ExecutionPolicy Bypass -File smart-copy.ps1
