d01aa4fbf815ef983a121df2da1193d392164498
Michel Vuijlsteke <michel@zog.org>
2025-05-29 17:01:52 +0200

    build

--- build.bat
+++ build.bat
@@ -7,4 +7,4 @@ git add *
 git commit -m "build"
 git push
 REM xcopy public \\files\mvuijlst\www\users /s /y
-robocopy public \\files\mvuijlst\www\users /E /Z /MT:4 /NDL /NJH /NJS
+robocopy public \\files\mvuijlst\www\users /MIR /Z /W:5 /R:3 /COPY:DT
