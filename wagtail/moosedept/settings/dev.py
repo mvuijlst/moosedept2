aec8c38042bd166ef45f8d514ca1799350d84c36
Michel Vuijlsteke <michel@zog.org>
2025-05-30 11:23:56 +0200

    Wagtail init

---
+++ wagtail/moosedept/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-=vrg$o3(c4w#tnn0&d@4)!$xp4h0dp$3!r^e^#!1r911d71q9-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
